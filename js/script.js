bac0e1e0e91979f4648e4a54a6b7837142430656
Lenovo <priyangspatel12345@gmail.com>
2021-02-22 01:06:48 +0530

    upto gallery

---
+++ js/script.js
@@ -0,0 +1,19 @@
+jQuery(function() { 
+    lightbox.option({
+       
+        'wrapAround':true
+    })
+
+    $(window).scroll(function(){
+        let position =$(this).scrollTop();
+
+
+        if(position>=350){
+            $('.gallery').addClass('change');
+        }
+        else{
+            $('.gallery').removeClass('change');
+
+        }
+    })
+})
