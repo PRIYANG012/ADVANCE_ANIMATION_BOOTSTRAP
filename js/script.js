7c929623ca0f24b0c3e36590f522a1d57432d7cb
Lenovo <priyangspatel12345@gmail.com>
2021-02-26 23:26:50 +0530

    accordion complete

--- js/script.js
+++ js/script.js
@@ -16,4 +16,15 @@ jQuery(function() {
 
         }
     })
+
+
+
+    $('.places-accordion').click(function(event){
+        if(event.target.id.split('_')[0]=='button'){
+            $('#placedisplay1').attr('src','/images/'+event.target.id.split('_')[1]+'-1.jpg')
+            $('#placedisplay2').attr('src','/images/'+event.target.id.split('_')[1]+'-2.jpg')
+            
+        };
+
+    })
 })
